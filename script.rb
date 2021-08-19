c5c3b8797d9c338660945edaed3c02110658d159
Andy Whetstone <andy_whetstone@live.com>
2021-08-18 20:00:42 -0500

    final commit

--- script.rb
+++ script.rb
@@ -1,3 +1,17 @@
+require 'pry-byebug'
+
 def substrings(word, dictionary)
-    
+    result = Hash.new(0)
+    word = word.downcase
+
+    dictionary.each do |text|
+        match = word.scan(text).length
+        result[text] = match unless match == 0
+    end
+
+    return result
 end
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+puts substrings("Howdy partner, sit down! How's it going?", dictionary)
